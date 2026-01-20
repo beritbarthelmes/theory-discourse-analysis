9c9faa442b79e03b49d5942b6c6932a543573740
beritbarthelmes <79358423+beritbarthelmes@users.noreply.github.com>
2026-01-20 00:02:13 -0600

    Update 01_search_crossref.py
    Harden Crossref PDF retrieval script

--- scripts/01_search/01_search_crossref.py
+++ scripts/01_search/01_search_crossref.py
@@ -1,41 +1,67 @@
 """
 Project: Theory Discourse Analysis
-Retrieve available PDF links from Crossref for a list of DOIs and download them via a browser-based workflow
+Retrieve available PDF links from Crossref for a list of DOIs and download them
+via a browser-based workflow.
+
+Outputs:
+- PDFs downloaded via the system browser (location determined by browser settings)
+
+Notes:
+- Uses Selenium to respect publisher-controlled download mechanisms
+- Requires local Chrome installation and compatible ChromeDriver
+- Intended for manual or semi-automated use due to copyright restrictions
 """
 
 from crossref.restful import Works
 from selenium import webdriver
 from time import sleep
 
-# build web driver, which operates on Chrome automatically, visible
+
 def get_driver():
-  options = webdriver.ChromeOptions()
-  options.add_argument('--disable-logging')
-  options.add_argument('--ignore-certificate-errors')
-  options.add_argument('--ignore-ssl-errors')
-  options.add_experimental_option('prefs',  {
-    "download.prompt_for_download": False,
-    "download.directory_upgrade": True,
-    "plugins.always_open_pdf_externally": True
-    }
-  )
-  driver = webdriver.Chrome(options=options)
-  return driver
+    """Initialize a visible Chrome browser for PDF downloading."""
+    options = webdriver.ChromeOptions()
+    options.add_argument('--disable-logging')
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--ignore-ssl-errors')
+    options.add_experimental_option(
+        'prefs',
+        {
+            "download.prompt_for_download": False,
+            "download.directory_upgrade": True,
+            "plugins.always_open_pdf_externally": True
+        }
+    )
+    driver = webdriver.Chrome(options=options)
+    return driver
 
-def fetch_articles_crossref(dois):
-  driver = get_driver()
 
-  works = Works()
-  for doi in dois:
-    article = works.doi(doi)
-    if article["link"]:
-      pdf_link = None
+def fetch_articles_crossref(dois, wait_time=3):
+    """
+    For each DOI, query Crossref for available PDF links and open them
+    in a browser to trigger downloads.
+    """
+    driver = get_driver()
+    works = Works()
 
-      try:
-        pdf_link = next(link for link in article["link"] if link["content-type"] == "application/pdf")
-      except:
-        pass
+    for doi in dois:
+        try:
+            article = works.doi(doi)
+        except Exception:
+            continue
 
-      if pdf_link:
-        driver.get(pdf_link["URL"])
-        sleep(3)
+        if not article or "link" not in article:
+            continue
+
+        try:
+            pdf_link = next(
+                link for link in article["link"]
+                if link.get("content-type") == "application/pdf"
+            )
+        except StopIteration:
+            pdf_link = None
+
+        if pdf_link:
+            driver.get(pdf_link["URL"])
+            sleep(wait_time)
+
+    driver.quit()
